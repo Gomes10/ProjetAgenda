3722d70891c52749dbeba4634957868d3e58a771
Timothée BARON <t1m95.tb@gmail.com>
2013-02-14 19:58:20 +0100

    Code source

---
+++ agenda.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<myconio.h>
+
+int choixIni=1,choixMenu;
+
+//procédure pour afficher le choix de départ
+void choixBase()
+{
+printf("Bienvenue dans l'application Agenda\n");
+printf("cette application a été créée dans le but de gérer vos contacts de facon la plus complete possible\n");
+
+    while(choixIni!=1 && choixIni!=2)
+    {
+        printf("Veuillez entrer votre choix : \n");
+        printf("1- Création d'un repertoire\n2- Ouverture d'un repertoire existant");
+        scanf("%d",&)
+    }
+
+}
+
+//
+void creationAgenda()
+{
+
+}
+
+void ouvertureAgenda()
+{
+
+}
+
+void affichageMenu()
+{
+
+}
+
+void creaContact()
+{
+
+}
+
+void rechercheContact()
+{
+
+}
+
+void suppContact()
+{
+
+}
+
+void afficheContact()
+{
+
+}
+
+void nbContact()
+{
+
+}
+
+void cryptage()
+{
+
+}
+
+void deCryptage()
+{
+
+}
+
+
+main()
+{
+    FILE *pf;  //à voir pour l'utiliser en variable globale
+
+    //si choixIni = 1 alors appel creationAgenda sinon appel ouvertureAgenda
+
+
+
+
+}
